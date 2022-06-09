19ee62aa13e1bb111098c66b22cff3f1292f963d
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-09 20:59:53 +0100

    Add style to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Header = ({ id }) => <header id={id}><h1>Todos</h1></header>;
+const Header = ({ id }) => {
+  const headerStyle = {
+    padding: '20px 0',
+    lineHeight: '1.5em',
+  };
+
+  const headingStyle = {
+    fontSize: '6rem',
+    fontWeight: '600',
+    marginBottom: '2rem',
+    lineHeight: '1em',
+    color: '#ececec',
+    textTransform: 'lowercase',
+    textAlign: 'center',
+  };
+
+  return (
+    <header id={id} className={headerStyle}>
+      <h1 style={headingStyle}>Todos</h1>
+    </header>
+  );
+};
 
 Header.propTypes = {
   id: PropTypes.string.isRequired,
