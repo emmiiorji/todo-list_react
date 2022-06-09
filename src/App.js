00a1a67e5b74855f73fa4e239777f6da83f47f8a
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-09 21:43:24 +0100

    Add routing to pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import './App.css';
 import React from 'react';
-import TodoContainer from './components/TodoContainer';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
+import SingleAbout from './components/pages/SingleAbout';
 
-const App = () => <TodoContainer id="todo-container" />;
+const App = () => (
+  <>
+    <Navbar />
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="about/:slug" element={<SingleAbout />} />
+      <Route path="*" element={<NotFound />} />
+      <Route path="notfound" element={<NotFound />} />
+    </Routes>
+  </>
+);
 
 export default App;
